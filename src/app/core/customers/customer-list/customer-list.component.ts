7f5add9298e32adb95218115d4b58a8a914a5062
nicky.schobben@student.pxl.be <nicky.schobben@student.pxl.be>
2024-09-26 20:13:05 +0200

    Components toegevoegd

--- src/app/core/customers/customer-list/customer-list.component.ts
+++ src/app/core/customers/customer-list/customer-list.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {FilterComponent} from "../filter/filter.component";
+import {CustomerItemComponent} from "../customer-item/customer-item.component";
 
 @Component({
   selector: 'app-customer-list',
   standalone: true,
-  imports: [],
+  imports: [
+    FilterComponent,
+    CustomerItemComponent
+  ],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.css'
 })
